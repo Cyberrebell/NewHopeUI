550e60c925629328c20f1124c4375c537bc313d2
Cyberrebell <chainsaw75@web.de>
2025-04-14 20:14:58 +0200

    target frames for mobs added

--- TargetFrame/TargetBar.lua
+++ TargetFrame/TargetBar.lua
@@ -1,7 +1,7 @@
 NHTargetBar = {}
 
 function NHTargetBar:PostClick(frame, button, down)
-    SendChatMessage(frame.target.name, "SAY")
+    SendChatMessage(frame.targetName, "SAY")
     frame:SetChecked(0)
 end
 
@@ -9,7 +9,13 @@ function NHTargetBar:OnUpdate(frame, elapsed)
     if frame.target then
         local hp = frame.target.hp.value
         local maxHP = frame.target.hp.max
-        frame.text:SetText(frame.target.name.." "..hp.." / "..maxHP..(" (%.1f"):format(100 * hp / maxHP).."%)")
+        local name = frame.target.name
+        if frame.smallmode then
+            name = string.sub(name, 0, 22).."\n"
+        else
+            name = name.." "
+        end
+        frame.text:SetText(name..hp.."/"..maxHP..(" (%.1f"):format(100 * hp / maxHP).."%)")
         frame:SetMinMaxValues(0, maxHP)
         frame:SetValue(hp)
     else
@@ -17,8 +23,20 @@ function NHTargetBar:OnUpdate(frame, elapsed)
     end
 end
 
+function NHTargetBar:SetSmallMode(frame, value)
+    frame.smallmode = value
+    if frame.smallmode then
+        local font, _, flags = frame.text:GetFont()
+        frame.text:SetFont(font, 8, flags)
+    end
+end
+
 function NHTargetBar_OnLoad(frame)
     frame.text = _G[frame:GetName().."Text"]
+    function frame:SetWidthRec(width)
+        frame:SetWidth(width)
+        _G[frame:GetName().."Button"]:SetWidth(width)
+    end
     function frame:SetUnit(unit)
         frame.target = unit
         local button = _G[frame:GetName().."Button"]
--- TargetFrame/TargetFrame.lua
+++ TargetFrame/TargetFrame.lua
@@ -5,16 +5,28 @@ function NHTargetFrame:updateTargets()
     for GUID, unit in pairs(NHEnemyDB) do
         if i < 5 then
             _G["NHBoss"..i]:SetUnit(unit)
+        elseif i < 21 then
+            _G["NHMob"..(i - 4)]:SetUnit(unit)
         end
         i = i + 1
     end
 end
 
-function NHTargetFrame_OnLoad(self)
+local function initTargetFrames()
     for i = 0,3 do
         local bar = _G["NHBoss"..(i + 1)]
         bar:SetPoint("TOP", 0, i * -16)
     end
+    for i = 0,19 do
+        local bar = _G["NHMob"..(i + 1)]
+        bar:SetWidthRec(100)
+        bar:SetPoint("TOP", 0, math.floor(i / 4) * -16)
+        NHTargetBar:SetSmallMode(bar, true)
+    end
+end
+
+function NHTargetFrame_OnLoad(self)
+    initTargetFrames()
     table.insert(NHEnemyDBEvents.onEnemyAdded, function (GUID)
         NHTargetFrame:updateTargets()
     end)
